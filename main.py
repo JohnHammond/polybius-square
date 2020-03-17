49f7200ef573e65e689955886ecbf37c9668f3f6
John Hammond <johnhammond010@gmail.com>
2020-03-17 14:24:12 -0400

    Corrected loops to include the last row in the square. Made operations function return rather than print.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 # Polybius Square [Square 5x5, Removed Letter: 'W']
 # University of Djillali Liabes (UDL) - Sidi Bel Abbes
-# Faculty of Sciences - Computer Science (LMD1) 2011/2012 
+# Faculty of Sciences - Computer Science (LMD1) 2011/2012
 # Student: Boubakr NOUR <n.boubakr@gmail.com>
 
 # License: Academic Free License (AFL)
@@ -13,14 +13,17 @@ import os
 
 from polybius import Polybius
 
+
 def clear():
-	"""Function to clear the Terminal (Console), works under Windows, Mac OS, *Unix"""
-	os.system(['clear', 'cls'][os.name == 'nt'])
+    """Function to clear the Terminal (Console), works under Windows, Mac OS, *Unix"""
+    os.system(["clear", "cls"][os.name == "nt"])
+
 
 def main_menu():
-	"""Procedure to display the main menu"""
-	clear()
-	print ("""
+    """Procedure to display the main menu"""
+    clear()
+    print(
+        """
 	| * * * WELCOME TO THE POLYBIUS SQUARE ENCIPHER/DECIPHER * * * |
 	 __________________________ MAIN MENU _________________________
 	|                                                              |
@@ -31,12 +34,15 @@ def main_menu():
 	|    (*) To quit the script, Press <Q>                         |
 	|                                                              |
 	|______________________________________________________________| 
-	""")
+	"""
+    )
+
 
 def instructions():
-	"""Procedure to display the script instructions."""
-	clear()
-	print ("""
+    """Procedure to display the script instructions."""
+    clear()
+    print(
+        """
 	 ________________________ INSTRUCTIONS ________________________
 	|                                                              |
 	|                                                              |
@@ -51,12 +57,15 @@ def instructions():
 	|       So "A"=11, "E"=15, "U"=51 ...                          |
 	|                                                              |
 	|______________________________________________________________|
-	""")
+	"""
+    )
+
 
 def quit():
-	"""Procedure to display quit message"""
-	clear()
-	print ("""
+    """Procedure to display quit message"""
+    clear()
+    print(
+        """
 	 _________ QUIT THE POLYBIUS SQUARE ENCIPHER/DECIPHER _________
 	|                                                              |
 	|                                                              |
@@ -70,12 +79,15 @@ def quit():
 	|  (*) Student: Boubakr NOUR <n.boubakr@gmail.com>             |
 	|                                                              |
 	|______________________________________________________________|
-	""")
+	"""
+    )
 
-def error(msg=''):
-	"""Procedure to display an error message"""
-	clear()
-	print ("""
+
+def error(msg=""):
+    """Procedure to display an error message"""
+    clear()
+    print(
+        """
 	 ______                        _ 
 	|  ____|                      | |
 	| |__   _ __ _ __ ___  _ __   | |
@@ -84,63 +96,67 @@ def error(msg=''):
 	|______|_|  |_|  \___/|_|     (_)
 	
 
-	""", msg)
+	""",
+        msg,
+    )
+
 
 def main():
-	main_menu()	# Main Menu
-	myPolybius = Polybius()
+    main_menu()  # Main Menu
+    myPolybius = Polybius()
 
-	try:
-		main_choice = input('[+] Enter your choice: ').lower().strip()
-	except:
-		main()
-	
-	if main_choice == 'i':		# Instructions
-		instructions()
-		
-		input('Press <Enter> to return to the main menu... ')
-		main()
-		
-	elif main_choice == 'e':	# Encipher
-		clear()
-		print("\v\tEnter your plaintext to encipher: ")
-		try:
-			sentence = input("--> ")
-		except KeyboardInterrupt:
-			main()
+    try:
+        main_choice = input("[+] Enter your choice: ").lower().strip()
+    except:
+        main()
 
-		print("\v\tYour ciphertext is: \n")
-		myPolybius.encipher(sentence)
-		
-		input('\n\vPress <Enter> to return to the main menu... ')
-		main()
-	
-	elif main_choice == 'd':	# Decipher
-		clear()
-		print("\v\tEnter your ciphertext to decipher: ")
+    if main_choice == "i":  # Instructions
+        instructions()
 
-		try:
-			sentence = input("--> ")
-		except KeyboardInterrupt:
-			main()
-		
-		print("\v\tYour message decrypted is: ")
-		myPolybius.decipher(sentence)
+        input("Press <Enter> to return to the main menu... ")
+        main()
 
-		input('\n\nPress <Enter> to return to the main menu... ')
-		main()
-	
-	elif main_choice == 'q':	# Quit
-		quit();
-		input("	----------------------------------------------------------------")
-		exit(0)
-	
-	else:
-		error("I didn't understand your choice !\n")
-		input('Press <Enter> to try again... ')
-		main()
+    elif main_choice == "e":  # Encipher
+        clear()
+        print("\v\tEnter your plaintext to encipher: ")
+        try:
+            sentence = input("--> ")
+        except KeyboardInterrupt:
+            main()
 
-if __name__ == '__main__':
-	main()
+        print("\v\tYour ciphertext is: \n")
+        print(myPolybius.encipher(sentence))
+
+        input("\n\vPress <Enter> to return to the main menu... ")
+        main()
+
+    elif main_choice == "d":  # Decipher
+        clear()
+        print("\v\tEnter your ciphertext to decipher: ")
+
+        try:
+            sentence = input("--> ")
+        except KeyboardInterrupt:
+            main()
+
+        print("\v\tYour message decrypted is: ")
+        print(myPolybius.decipher(sentence))
+
+        input("\n\nPress <Enter> to return to the main menu... ")
+        main()
+
+    elif main_choice == "q":  # Quit
+        quit()
+        input("	----------------------------------------------------------------")
+        exit(0)
+
+    else:
+        error("I didn't understand your choice !\n")
+        input("Press <Enter> to try again... ")
+        main()
+
+
+if __name__ == "__main__":
+    main()
 
 # EOF
